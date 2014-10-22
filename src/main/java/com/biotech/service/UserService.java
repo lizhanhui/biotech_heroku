8963ab1827facc4c40e92c1392510f332f0263ba
Zhanhui Li <lizhanhui@gmail.com>
2014-10-23 02:53:57 +0800

    add topN APi

--- src/main/java/com/biotech/service/UserService.java
+++ src/main/java/com/biotech/service/UserService.java
@@ -2,6 +2,11 @@ package com.biotech.service;
 
 import com.biotech.model.User;
 
+import java.util.List;
+
 public interface UserService {
+
     public User get(long id);
+
+    public List<User> topN(int n);
 }
--- src/main/java/com/biotech/service/impl/UserServiceImpl.java
+++ src/main/java/com/biotech/service/impl/UserServiceImpl.java
@@ -7,6 +7,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import java.util.ArrayList;
+import java.util.List;
 
 @Produces("application/json")
 public class UserServiceImpl implements UserService {
@@ -20,5 +22,27 @@ public class UserServiceImpl implements UserService {
         user.setName("Test-Name");
         user.setEmail("user@company.com");
         return user;
+    }
+
+
+    @GET
+    @Path("/users/topN/{n}")
+    @Override
+    public List<User> topN(@PathParam("n") int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Top N directly implies N > 0");
+        }
+
+        List<User> userList = new ArrayList<User>(n);
+        User user = null;
+        for (int i = 0; i < n; i++) {
+            user = new User();
+            user.setId(i);
+            user.setName("User " + i);
+            user.setEmail("user" + i + "@company.com");
+            userList.add(user);
+        }
+
+        return userList;
     }
 }
